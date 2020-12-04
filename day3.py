5e50b2b114876e99a9d1904a20f3ff24600cfb83
Anthony Bouvier <thebouv@gmail.com>
2020-12-04 14:49:03 -0600

    Adds day3 part 1 and 2 .. not fun lol

---
+++ day3.py
@@ -0,0 +1,44 @@
+with open('day3.txt') as file:
+    lines = file.read().splitlines()
+
+
+def main():
+    slope1 = findtrees(1, 1)
+    slope2 = findtrees(3, 1)
+    slope3 = findtrees(5, 1)
+    slope4 = findtrees(7, 1)
+    slope5 = findtrees(1, 2)
+
+    print(slope1*slope2*slope3*slope4*slope5)
+
+
+def findtrees(rateX, rateY):
+    xpos = 0
+    ypos = 0
+    trees = 0
+    maxpos = len(lines[0]) - 1
+    botpos = len(lines) - 1
+
+    while ypos < botpos:
+        xpos += rateX
+        ypos += rateY
+
+        if ypos > botpos:
+            break
+
+        if xpos > maxpos:
+            xpos = xpos - maxpos - 1
+
+        if lines[ypos][xpos] == '#':
+            trees += 1
+
+    return trees
+
+
+if __name__ == "__main__":
+    main()
+
+# print(f"{xpos=}")
+# print(f"{ypos=}")
+# print(f"{botpos=}")
+# print(f"{trees=}")
