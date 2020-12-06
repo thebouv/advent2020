babc17e54aafc14680de551d910f7582943024e5
Anthony Bouvier <thebouv@gmail.com>
2020-12-06 13:20:43 -0600

    intersections baby in part two

--- day6.py
+++ day6.py
@@ -5,6 +5,7 @@ def main():
     groups = text.split("\n\n")
 
     print(f"day 5 part one: {partone(groups)}")
+    print(f"day 5 part two: {parttwo(groups)}")
 
 
 def partone(groups):
@@ -14,6 +15,16 @@ def partone(groups):
         group = group.replace("\n", "")
         total_count += len(set(group))
 
+    return total_count
+
+
+def parttwo(groups):
+    total_count = 0
+
+    for group in groups:
+        group = group.split("\n")
+        group = list(map(set, group))
+        total_count += len(group[0].intersection(*group))
     return total_count
 
 
